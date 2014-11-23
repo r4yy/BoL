4063f9594b492a36ee7f1c78e1951994f3af48a4
r4yy <l0st@live.de>
2014-11-23 16:20:02 +0100

    Update RedCapAnnie.lua

--- RedCapAnnie.lua
+++ RedCapAnnie.lua
@@ -1,20 +1,27 @@
+local version = "0.1"
 if myHero.charName ~= "Annie" then return end
 
---> Credits: Fantastik - helped me a lot
+--[[ 
+		RedCapAnnie v.04
+		by r4Y aka l0ST
+		
+		Credits: Fantastik - helped me a lot with this script	
+--]]
+
+---> Auto downloader/updater <----
 
 local SOURCELIB_URL = "https://raw.github.com/TheRealSource/public/master/common/SourceLib.lua"
 local SOURCELIB_PATH = LIB_PATH.."SourceLib.lua"
 
 local updateScript = true
 local scriptName = "RedCapAnnie"
-local version = 0.3
 local host = "raw.github.com"
 local updatePath = "/r4yy/BoL/master/" .. scriptName .. ".lua"
 local filePath = SCRIPT_PATH .. GetCurrentEnv().FILE_NAME
 local versionPath = "/r4yy/BoL/master/version/" .. scriptName .. ".version"
 local url = "https://"..host..updatePath
 
---> Check for update and download it
+--> Check script for update and download it
 function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>RedCapAnnie:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if updateScript then
 	local ServerData = GetWebResult(host, versionPath)
@@ -34,6 +41,12 @@ if updateScript then
 	end
 end
 --<
+
+
+
+
+
+
 --> Check for Vpre and SOW and download them
 local required_files =
 		{
@@ -67,124 +80,62 @@ function deCounter()
 end	
 --<
 
---////////////////////////////////////////////////////////////////////////////////////////////
---////////////////////////////////////////////////////////////////////////////////////////////
---////////////////////////////////////////////////////////////////////////////////////////////
---////////////////////////////////////////////////////////////////////////////////////////////
---////////////////////////////////////////////////////////////////////////////////////////////
 
-local target = nil
-local ts
-local ts = TargetSelector(TARGET_LESS_CAST_PRIORITY, qRange, DAMAGE_MAGIC, true)
+
+--> Char Data 
+local qRange, wRange, rRange = 625, 625, 600
+local qWidth, wWidth, rWidth = -1, 50*math.pi/180, 250
+local qDelay, wDelay, rDelay = 0.25, 0.25, 0.25
+local qSpeed, wSpeed, rSpeed = 1400, math.huge, math.huge
+local qReady, wReady, rReady = false, false, false
+local igSlot, igReady = nil, false
+
+local spellLevel = 0
+
+--> Ow Data 
 local VP = nil
 
-function OnLoad()	
-	VP = VPrediction()
-	SOW = SOW(VP)
+local trueRange = 0
+local useSAC, useSOW, useSxOW = false, false false
+
+local tick = 0
+local checkedMMASAC = false
+local is_MMA = false
+local is_REVAMP = false
+local is_REBORN = false
+local is_SAC = false
+----------------------------------------------------
+
+
+
+--> Target Data
+local ts = nil
+local Target, currTargetPos = nil, nil
+--------------------------------------<
+
+
+
+
+
+function OnLoad()
+	tick = GetTickCount()
 	Menu()
-	Annie()
-	print("ReCapAnnie by r4YY successfully loaded!")
+	chooseP()
+	Ts()
+	PrintChat("<font color='#e62519'> >> "..scriptName.." v."..version.." by r4Y loaded!</font>")	
 end
 
-function OnTick ()
+function OnTick()
 	if myHero.dead then return end
-	ts:update()
-	
-	if myHero.combo.useCombo then
-		Combo()
-	end
+	checkOW()
+	Checks()
 end
 
-function Annie()
-		qRange, wRange, rRange = 625, 625, 600
-		qWidth, wWidth, rWidth = -1, 50*math.pi/180, 250
-		qDelay, wDelay, rDelay = 0.25, 0.25, 0.25
-		qSpeed, wSpeed, rSpeed = 1400, math.huge, math.huge
-		
-		qReady = (myHero:CanUseSpell(_Q) == READY)
-		wReady = (myHero:CanUseSpell(_W) == READY)
-		rReady = (myHero:CanUseSpell(_R) == READY)
-		igReady = (ignite ~= nil and myHero:CanUseSpell(ignite) == READY)
-		
-		skillRQWE = {1,2,3,1,1,4,1,2,1,2,4,2,2,3,3,4,3,3}
-		skillRWQE = {2,1,3,2,2,4,2,1,2,1,4,1,1,3,3,4,3,3}
+function OnDraw()
+	drawRanges()
 end
 
---[[function SkinHack()
-	if myMenu.misc.changeSkin.enabled and CurSkin ~= myMenu.misc.changeSkin.skinNo then
-		local skinNoChange = { [1] = 7, [2] = 1, [3] = 2, [4] = 3, [5] = 4, [6] = 5, [7] = 6 }
-		CurSkin = myMenu.misc.changeSkin.skinNo
-		SkinChanger(myHero.charName, skinNoChange[CurSkin])
-	end
-end
---]]
 
-function Combo()
-	if ValidTarget(ts.target) then
-		if myMenu.combo.comboMode == 1 then
-			if qReady and myMenu.combo.useQ then
-				CastSpell(_Q, ts.target.x, ts.target.z)
-			end
-			if wReady and myMenu.combo.useW then
-				castW(ts.target)
-			end
-			if rReady and myMenu.combo.useR then
-				castR(ts.target)
-			end
-		end
-		if myMenu.combo.comboMode == 2 then
-			if wReady and myMenu.combo.useW then
-				castW(ts.target)
-			end		
-			if qReady and myMenu.combo.useQ then
-				CastSpell(_Q, ts.target.x, ts.target.z)
-			end
-			if rReady and myMenu.combo.useR then
-				castR(ts.target)
-			end
-		end
-		if myMenu.combo.comboMode == 3 then
-			if rReady and myMenu.combo.useR then
-				castR(ts.target)
-			end			
-			if wReady and myMenu.combo.useW then
-				castW(ts.target)
-			end		
-			if qReady and myMenu.combo.useQ then
-				CastSpell(_Q, ts.target.x, ts.target.z)
-			end	
-		end
-		if myMenu.combo.comboMode == 4 then
-			if rReady and myMenu.combo.useR then
-				castR(ts.target)
-			end		
-			if qReady and myMenu.combo.useQ then
-				CastSpell(_Q, ts.target.x, ts.target.z)
-			end				
-			if wReady and myMenu.combo.useW then
-				castW(ts.target)
-			end		
-		end
-	end
-end
-
-function castW(target)
-	for i, target in pairs(GetEnemyHeroes()) do
-			CastPosition,  HitChance,  Position = VP:GetLineCastPosition(myHero, wDelay, wWidth, wRange, wSpeed, myHero, false)
-				if HitChance >= 2 or 3 and GetDistance(CastPosition) < wRange - 50 then 
-					CastSpell(_W, CastPosition.x, CastPosition.z)
-				end
-	end		
-end	
-				
-function castR(target)
-	for i, target in pairs(GetEnemyHeroes()) do
-			CastPosition, HitChance, Position = VP:GetCircularAOECastPosition(target, rDelay, rWidth, rRange, rSpeed, myHero)
-				if HitChance >= 2 or 3 and GetDistance(CasPosition) < 600 then
-					CastSpell(_R, CastPosition.x, CasPosition.z)
-				end
-	end
-end	
 
 
 function Menu()
@@ -194,7 +145,6 @@ function Menu()
 		SOW:LoadToMenu(myMenu.Orbwalking)
 	
 	myMenu:addSubMenu("Target selector", "ts")
-		ts:AddToMenu(myMenu.ts)	
 
 	myMenu:addSubMenu("Annie - Combo Settings", "combo")
 		myMenu.combo:addParam("useQ", "Use Q in Combo", SCRIPT_PARAM_ONOFF, false)
@@ -219,16 +169,110 @@ function Menu()
 		myMenu.farm:addParam("LaneClear", "Farm LaneClear", SCRIPT_PARAM_ONKEYDOWN, false,   string.byte("V"))
 		
 	myMenu:addSubMenu("Annie - Draw Settings", "drawing")
-		myMenu.drawing:addParam("drawQ", "Draw Circle Q", SCRIPT_PARAM_ONOFF, false)
-		myMenu.drawing:addParam("drawW", "Draw Circle W", SCRIPT_PARAM_ONOFF, false)
+		myMenu.drawing:addParam("drawQ", "Draw Circle Q/W", SCRIPT_PARAM_ONOFF, false)
 		myMenu.drawing:addParam("drawR", "Draw Circle R", SCRIPT_PARAM_ONOFF, false)
 		myMenu.drawing:addParam("drawAA", "Draw Circle AA", SCRIPT_PARAM_ONOFF, false)
 		
 	myMenu:addSubMenu("Annie - Misc Settings", "misc")
 		myMenu.misc:addSubMenu("Auto level spells", "lvlSkill")
 		myMenu.misc.lvlSkill:addParam("enable", "Auto Level Skills", SCRIPT_PARAM_ONOFF, false)
-		myMenu.misc.lvlSkill:addParam("skill", "Order", SCRIPT_PARAM_LIST, 1, {"R>W>Q>E", "R>Q>W>E"})
+		myMenu.misc.lvlSkill:addParam("skillOrder", "Order", SCRIPT_PARAM_LIST, 1, {"R>W>Q>E", "R>Q>W>E", "R>E>W>Q", "R>E>Q>W"})
 		myMenu.misc:addSubMenu("SkinChanger for VIPs", "skinChanger")
 		myMenu.misc.skinChanger:addParam("enable", "SkinChanger", SCRIPT_PARAM_ONOFF, false)		
 		myMenu.misc.skinChanger:addParam("skinNo", "Choose your model", SCRIPT_PARAM_LIST, 1, { "Classic Skin", "Goth Annie", "Red Riding Annie", "Annie in Wonderland", "Prom Queen Annie", "Frostfire Annie", "Franken Tibbers Annie", "Reverse Annie", "Panda Annie"})
 end
+
+function chooseP()
+
+end
+
+function Ts()
+	ts = TargetSelector(TARGET_LESS_CAST_PRIORITY, qRange)
+	ts.name = "Annie"
+	myMenu.ts:addTS(ts)
+end
+
+function checkOW()
+
+end
+
+function Checks()
+	ts:update()
+	qReady = (myHero:CanUseSpell(_Q) == READY)
+    wReady = (myHero:CanUseSpell(_W) == READY)
+    rReady = (myHero:CanUseSpell(_R) == READY)
+	trueRange = myHero.range + (GetDistance(myHero.minBBox) - 5)
+	target = owTarget()
+	autoLevelSkills()
+end
+
+function owTarget()
+
+end
+
+function autoLevelSkills()
+	if not myMenu.misc.lvlSkill.enable then return end
+	if myHero.level > spellLevel then
+		spellLevel = spellLevel + 1
+		if myMenu.misc.lvlSkill.skillOrder == 1 then			
+			LevelSpell(_R)
+			LevelSpell(_W)
+			LevelSpell(_Q)
+			LevelSpell(_E)
+		end
+		if myMenu.misc.lvlSkill.skillOrder == 2 then	
+			LevelSpell(_R)
+			LevelSpell(_Q)
+			LevelSpell(_W)
+			LevelSpell(_E)
+		end
+		if myMenu.misc.lvlSkill.skillOrder == 3 then	
+			LevelSpell(_R)
+			LevelSpell(_E)
+			LevelSpell(_W)
+			LevelSpell(_Q)
+		end
+		if myMenu.misc.lvlSkill.skillOrder == 4 then	
+			LevelSpell(_R)
+			LevelSpell(_E)
+			LevelSpell(_Q)
+			LevelSpell(_W)
+		end
+	end
+end
+
+function drawRanges()
+	if myMenu.drawing.drawQ then
+		DrawCircle(myHero.x, myHero.y, myHero.z, qRange, ARGB(35 , 105, 105, 105))
+	end
+	if myMenu.drawing.drawR then
+		DrawCircle(myHero.x, myHero.y, myHero.z, rRange, ARGB(75 , 185, 185, 185))
+	end
+	if myMenu.drawing.drawAA then
+		DrawCircle(myHero.x, myHero.y, myHero.z, trueRange, ARGB(55 , 150, 150, 150))
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
